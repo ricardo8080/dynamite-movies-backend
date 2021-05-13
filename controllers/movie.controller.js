3237f4df796c5ae9532264f268fddec83ddaf122
Ricardo <ricardo.fernandez.bo.2000@ieee.org>
2021-05-13 13:27:38 -0400

    missing checkers in getsearchfilteredmovielist method

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -26,14 +26,20 @@ MovieCtrl.getSearchFliteredMovieList = async (req,res) => {
     console.log(req.header('genres')+ typeof(req.header('genres')));
     let Movies = await Movie.find();
     Movies = Movies.filter((item) => {
+            if(req.header('nameMovie') === undefined || 
+               req.header('nameMovie') === null){
+                return true;
+            }
             return item.nameMovie.includes(req.header('nameMovie'))
         }).filter((item) => {
             return item.country === req.header('country') || 
-                   req.header('country') === undefined
+                   req.header('country') === undefined || 
+                   req.header('country') === null
         }).filter((item) => {
             const gendersRequested = req.header('genres');
             let hasGenders = true;
-            if(req.header('genres') === undefined){
+            if(req.header('genres') === undefined || 
+               req.header('genres') === null){
                 return true;
             } else {
                 const genderSplit = gendersRequested.split(",");
