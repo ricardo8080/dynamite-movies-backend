ba89fdcc1a23a420af094b0c22035f472a229929
anditavillarroel99 <anditavillarroel99@gmail.com>
2021-05-13 17:59:02 -0400

    Modify the filter method

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -27,19 +27,21 @@ MovieCtrl.getSearchFliteredMovieList = async (req,res) => {
     let Movies = await Movie.find();
     Movies = Movies.filter((item) => {
             if(req.header('nameMovie') === undefined || 
-               req.header('nameMovie') === null){
+               req.header('nameMovie') === null ||
+               req.header('nameMovie') === '') {
                 return true;
             }
             return item.nameMovie.includes(req.header('nameMovie'))
         }).filter((item) => {
             return item.country === req.header('country') || 
-                   req.header('country') === undefined || 
-                   req.header('country') === null
+                  req.header('country') === undefined || 
+                  req.header('country') === null ||
+                  req.header('country') === ''
         }).filter((item) => {
             const gendersRequested = req.header('genres');
             let hasGenders = true;
             if(req.header('genres') === undefined || 
-               req.header('genres') === null){
+               req.header('genres') === null || req.header('genres') === ''){
                 return true;
             } else {
                 const genderSplit = gendersRequested.split(",");
